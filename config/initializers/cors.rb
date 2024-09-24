7235df82f9941a400e3dc10ec080019eeeb2d2fc
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2024-09-24 22:10:15 +0530

    fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'http://localhost:5174'
+    origins '*'
 
     resource '*',
              headers: :any,
